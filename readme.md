72f860c5ba81c3bef5db8c58f4a0d3aa37c62a67
Haris Sucipto <harisssucipto@gmail.com>
2017-04-11 22:49:58 +0700

    creating web server

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Belajar React JS
+cara bikin server:
+
+* ini dulu npm
+```npm init```
+
+* misalnya pakai express versi 4 lalu diembedkan (--save) ke pengaturan packge.json dengan perintah ini
+```npm install express@4 --save ```
+
+* sisanya liat source code
---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+// Create our app
+var app = express();
+
+// specifiy folder to use for default path
+app.use(express.static('public'));
+
+// port to launch
+app.listen(3000, function() {
+  console.log('Express is up on port 3000');
+});
